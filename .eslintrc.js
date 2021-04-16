86b9e15645cacb56070b38203b9718007644c8ce
jasba24 <johanbuitrago342@gmail.com>
2021-04-16 15:38:19 -0500

    create the component for the app and the router

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,17 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/prettier",
+    "prettier",
+  ],
   parserOptions: {
     parser: "babel-eslint",
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    semi: "off",
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
+import Vue from "vue"
+import App from "./App.vue"
+import "@/assets/css/tailwind.css"
 
-Vue.config.productionTip = false;
+import router from "@/router"
+
+Vue.config.productionTip = false
 
 new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+	router,
+	render: h => h(App),
+}).$mount("#app")
