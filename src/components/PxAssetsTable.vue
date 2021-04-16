d168e0db9db49f8df2e0732a7cc1ba2d1b80c9b7
jasba24 <johanbuitrago342@gmail.com>
2021-04-16 18:09:54 -0500

    new button for show the details of the icon

--- src/components/PxAssetsTable.vue
+++ src/components/PxAssetsTable.vue
@@ -30,28 +30,39 @@
 					<b>#{{ a.rank }}</b>
 				</td>
 				<td>
-					{{ a.name }}
+					<router-link
+						class="hover:underline text-green-600"
+						:to="{ name: 'coin-detail', params: { id: a.id } }"
+					>
+						{{ a.name }}
+					</router-link>
+					<small class="ml-1 text-gray-500">{{ a.symbol }}</small>
 				</td>
 				<td>{{ a.priceUsd | dollar }}</td>
 				<td>{{ a.marketCapUsd | dollar }}</td>
 				<td
-					:class="
-						a.changePercent24Hr.includes('-')
-							? 'text-red-600'
-							: 'text-green-600'
-					"
+					:class="a.changePercent24Hr.includes('-') ? 'text-red-600' : 'text-green-600'"
 				>
 					{{ a.changePercent24Hr | percent }}
 				</td>
-				<td class="hidden sm:block"></td>
+				<td class="hidden sm:block">
+					<px-button @custom-click="goToCoin(a.id)">
+						<span>Detalle</span>
+					</px-button>
+				</td>
 			</tr>
 		</tbody>
 	</table>
 </template>
 
 <script>
+import PxButton from "@/components/PxButton"
 export default {
 	name: "PxAssetsTable",
+
+	components: {
+		PxButton,
+	},
 
 	props: {
 		assets: {
@@ -59,6 +70,12 @@ export default {
 			default: () => [],
 		},
 	},
+
+	methods: {
+		goToCoin(id) {
+			this.$router.push({ name: "coin-detail", params: { id } })
+		}
+	}
 }
 </script>
 
--- src/components/PxHeader.vue
+++ src/components/PxHeader.vue
@@ -4,8 +4,10 @@
 			<nav class="flex items-center justify-between flex-wrap bg-green-400 p-6">
 				<div class="flex items-center flex-shrink-0 text-white mr-6">
 					<px-icon class="mr-2" />
-					<span class="font-semibold text-xl tracking-tight"
-						>PlatziExchange</span
+					<router-link
+						:to="{ name: 'home' }"
+						class="font-semibold text-xl tracking-tight"
+						>PlatziExchange</router-link
 					>
 				</div>
 				<div
