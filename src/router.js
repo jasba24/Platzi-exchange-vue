d4552419f51dfe45708ce6083d9025929e2e1485
jasba24 <johanbuitrago342@gmail.com>
2021-04-16 17:46:04 -0500

    new route for handle the page of coinDetails and show info for every coin

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Router from "vue-router"
 import Home from "@/views/Home"
 import About from "@/views/About"
 import Error from "@/views/Error"
+import CoinDetail from "@/views/CoinDetail"
 
 Vue.use(Router)
 
@@ -25,6 +26,11 @@ export default new Router({
 			path: "*",
 			name: "Error",
 			component: Error,
+		},
+		{
+			path: "/coin/:id",
+			name: "coin-detail",
+			component: CoinDetail,
 		},
 	],
 })
