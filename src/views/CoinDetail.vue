2e9eb66292c0bf5c97f81752776b64f1c1b588de
jasba24 <johanbuitrago342@gmail.com>
2021-04-19 10:34:56 -0500

    add the list for show the Best Exchange Offers

--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -84,21 +84,52 @@
 				:max="max"
 				:data="history.map(h => [h.date, parseFloat(h.priceUsd).toFixed(2)])"
 			/>
+
+			<h3 class="text-xl my-10">Mejores Ofertas de Cambio</h3>
+			<table>
+				<tr
+					v-for="m in markets"
+					:key="`${m.exchangeId}-${m.priceUsd}`"
+					class="border-b"
+				>
+					<td>
+						<b>{{ m.exchangeId }}</b>
+					</td>
+					<td>{{ m.priceUsd || dollar }}</td>
+					<td>{{ m.baseSymbol }} / {{ m.quoteSymbol }}</td>
+					<td>
+						<px-button
+							:isLoading="m.isLoading || false"
+							v-if="!m.url"
+							@custom-click="getWebsite(m)"
+						>
+							<slot>Obtener link</slot>
+						</px-button>
+						<a v-else class="hover:underline text-green-600" target="_blanck">
+							{{ m.url }}
+						</a>
+					</td>
+				</tr>
+			</table>
 		</template>
 	</div>
 </template>
 
 <script>
+import PxButton from "@/components/PxButton"
 import api from "@/api"
 
 export default {
 	name: "CoinDetail",
+
+	components: { PxButton },
 
 	data() {
 		return {
 			isLoading: false,
 			asset: {},
 			history: [],
+			markets: [],
 		}
 	},
 
@@ -126,15 +157,31 @@ export default {
 	},
 
 	methods: {
+		getWebsite(exchange) {
+			this.$set(exchange, "isLoading", true)
+
+			return api
+				.getExchange(exchange.exchangeId)
+				.then(res => {
+					this.$set(exchange, "url", res.exchangeUrl)
+				})
+				.finally(() => (this.isLoading = false))
+		},
+
 		getCoin() {
 			const id = this.$route.params.id
 
 			this.isLoading = true
 
-			Promise.all([api.getAsset(id), api.getAssetHistory(id)])
-				.then(([asset, history]) => {
+			Promise.all([
+				api.getAsset(id),
+				api.getAssetHistory(id),
+				api.getMarkets(id),
+			])
+				.then(([asset, history, markets]) => {
 					this.asset = asset
 					this.history = history
+					this.markets = markets
 				})
 				.finally(() => (this.isLoading = false))
 		},
